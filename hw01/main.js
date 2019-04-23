ddf7e65874d47c142ac8219c828e5fa9d84e0383
Brian Belko <47993092+belko-b@users.noreply.github.com>
2019-04-23 02:01:02 -0400

    finished assignment

--- hw01/main.js
+++ hw01/main.js
@@ -8,21 +8,15 @@ reset_btn.addEventListener("click", reset);
 
 function calculate() {
     /*
-        Write your "calculation" code in this function.
-        1. Create variables to hold values from your form elements
-        2. Convert variables from Strings to Numbers if needed
-        3. Perform calculations
-        4. Call "output" as many times as needed.
+        These calculations take baseball statistics that are required to calculate the stat: On-Base Plus Slugging Percentage (OPS).
+        OPS represents the ability of a baseball player to both get on base and hit for power. These are two very important
+        abilities for a good player to have. The range in which the final result is found gives a good indication as to the 
+        quality of the player. 
         
-        Things to remember:
-        1. Name your variables meaningfully (with good spelling)
-        2. Write at least one multi-line comment that clearly explains in English what your calculations do
-        3. Each call to "output" will add a paragraph to your output. Use this to format your output neatly!
-        4. Don't forget that you need to edit index.html as well!
-        5. Although my example is nonsensical and is just intended to demonstrate what happens when you add Strings and Numbers together, yours should actually do something meaningful (or entertaining) that makes sense!
+        The second part of this OPS calculator outputs a message to the user informing them of how their calculated OPS stacks 
+        up to the range of results. This calculator is handy for both fans of baseball and players at any level of the game.
     */
 
-    /* These variables store the values from the form */
     let H = document.getElementById("totalHits").value;
     let BB = document.getElementById("totalWalks").value;
     let HBP = document.getElementById("totalHitByPitches").value;
@@ -33,44 +27,46 @@ function calculate() {
     let HR = document.getElementById("totalHomeruns").value;
     let AB = document.getElementById("totalAtBats").value;
     
-    /* These variables store the results of our calculations */
-    let c1 = H + BB + HBP;
-    let c2 = AB + BB + SF + HBP;
-    let c3 = (1 * B) + (2 * D) + (3 * T) + (4 * HR);
-    let c4 = c1 / c2;
-    let c5 = c4 + c3;
-    let OPS = c5 / AB;
     
+    let c1 = Number(H) + Number(BB) + Number(HBP);
+    let c2 = Number(AB) + Number(BB) + Number(SF) + Number(HBP);
+    let c3 = Number(B) * 1;
+    let c4 = Number(D) * 2;
+    let c5 = Number(T) * 3;
+    let c6 = Number(HR) * 4;
+    let c7 = c3 + c4 + c5 + c6;
+    let c8 = ((c1) / (c2) + (c7) / Number(AB));
+    let OPS = c8;
     
-    /* These function calls provide the output to our web page */
+
     output("Your OPS is: " + OPS);
     
     if (OPS >= .9000) {
-        output("You are an All-Star!")
+        output("You are an All-Star!");
     }
     
-    if (OPS.between (.8334, .8999) {
-        output("You are almost an All-Star!")
+    if (.8334 <= OPS && OPS <= .8999) {
+        output("You are almost an All-Star!");
     }
     
-    if (OPS.between (.7667, .8333)) {
-        output("You are an above average player!")
+    if (.7667 <= OPS && OPS <= .8333) {
+        output("You are an above average player!");
     }
     
-    if (OPS.between (.7000, .7666)) {
-        output("You are a perfectly average hitter!")
+    if (.7000 <= OPS && OPS <= .7666) {
+        output("You are a perfectly average hitter!");
     }
     
-    if (OPS.between (.6334, .6999)) {
-        output("You are below average but close to getting there.")
+    if (.6334 <= OPS && OPS <= .6999) {
+        output("You are below average but close to getting there.");
     }
     
-    if (OPS.between (.5667, .6333)) {
-        output("You are not very good I'm afraid.")
+    if (.5667 <= OPS && OPS <= .6333) {
+        output("You are not very good I'm afraid.");
     }
     
-    if (OPS.between (0, .5666)) {
-        output("You are a person who may want to consider coaching.")
+    if (0 <= OPS && OPS <= .5666) {
+        output("You are a person who may want to consider coaching.");
     }
 }
 
